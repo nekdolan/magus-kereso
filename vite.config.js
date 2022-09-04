8ef246984e23445de62c53387f2a9cfc3807a02b
Daniel <boros.daniel@protonmail.com>
2022-09-04 17:22:08 +0200

    added docs

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+const basePath = process.env.BUILD_BASE_PATH
+  ? { base: process.env.BUILD_BASE_PATH }
+  : {};
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  ...basePath,
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
